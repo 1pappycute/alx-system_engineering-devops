1bf0544ab732bb478a68c89ee3b6a39309607822
Tesleem <hassantesleem52@gmail.com>
2023-06-15 18:50:20 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited proprly, non-zero otherwise
+ */
+
+int main(0)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
